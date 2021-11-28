8a29180bc52f77f455c51b5a3d3265399e9bcdd4
GalSanyi <galsanyi84@gmail.com>
2021-11-28 21:04:57 +0100

    simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,59 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryEl = document.querySelector(".gallery");
+const imageMarkUp = galleryItems
+    .map(
+        (galleryItems) =>
+
+        `<a class="gallery__item" 
+    href="${galleryItems.original}">
+    <img class="gallery__image" 
+    src="${galleryItems.preview}" 
+    alt="${galleryItems.description}" />
+  </a>`
+
+    )
+    .join("");
+
+galleryEl.insertAdjacentHTML('beforeend', imageMarkUp);
+
+
+const gallery = new SimpleLightbox(".gallery a", {
+    captionSelector: "img",
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+
+});
+
+const onEsc = function(evt) {
+    if (evt.key === 'ESCAPE') {
+        instance.close();
+    };
+};
+
+
+
+const openGallery = function(evt) {
+    evt.preventDefault();
+
+
+    if (evt.target.nodeName !== "IMG") {
+        return;
+    }
+
+    instance.element().querySelector('img').src = evt.target.dataset.source;
+
+
+    instance.show()
+};
+
+
+galleryEl.addEventListener('click', openGallery);
