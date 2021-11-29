efb5bafe90828f757b05e1511567931a10700db5
GalSanyi <galsanyi84@gmail.com>
2021-11-29 20:48:09 +0100

    push

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+console.log(iframe);
 
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const onPlay = function(data) {
+    // data is an object containing properties specific to that event
+};
+
+player.on('play', onPlay);
