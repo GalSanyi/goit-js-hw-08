f70ed8581f34b558ef4bbf2f6689b5122611d6d9
GalSanyi <galsanyi84@gmail.com>
2021-12-01 17:15:09 +0100

    refs

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,49 +5,81 @@ const refs = {
     textarea: document.querySelector('.feedback-form textarea'),
     input: document.querySelector('.feedback-form input'),
 }
-const STORAGE_KEY = 'feedback-form-state';
-
-const formData = {};
-insertoftextarea();
 
 refs.form.addEventListener('submit', onFormSubmit);
-refs.textarea.addEventListener('input', throttle(onTextAreaInput, 500));
-
-refs.form.addEventListener('input', e => {
-
-    formData[e.target.name] = e.target.value; //забираем данные при вводе
-    // console.log(formData); //formData это обьэкт
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-});
+refs.form.addEventListener('input', throttle(onFormInput, 500));
 
 
+// const formData = {};
+// insertoftextarea();
 
-function onFormSubmit(evt) {
-    evt.preventDefault(); //не дает перегружать страницу
+// refs.form.addEventListener('input', e => {
 
-    evt.currentTarget.reset(); //очищяет форму
-    localStorage.removeItem(STORAGE_KEY); //очищяет localStorage
-    formData = {};
-}
+//     formData[e.target.name] = e.target.value; //забираем данные при вводе
+//     // console.log(formData); //formData это обьэкт
+//     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+// });
 
 
 
-function onTextAreaInput(evt) {
-    const message = evt.target.value;
-    localStorage.setItem(STORAGE_KEY, message);
+// function onFormSubmit(evt) {
+//     evt.preventDefault(); //не дает перегружать страницу
 
-}
-
-function insertoftextarea() {
-
-    const insertMessage = localStorage.getItem(STORAGE_KEY);
-    // const localParse = JSON.parse(insertMessage);
-
-    if (insertMessage) {
+//     evt.currentTarget.reset(); //очищяет форму
+//     localStorage.removeItem(STORAGE_KEY); //очищяет localStorage
+//     formData = {};
+// }
 
 
-        console.log(insertMessage);
-        refs.textarea.value = insertMessage;
+
+// function onTextAreaInput(evt) {
+//     const message = evt.target.value;
+//     localStorage.setItem(STORAGE_KEY, message);
+
+// }
+
+// function insertoftextarea() {
+
+//     const insertMessage = localStorage.getItem(STORAGE_KEY);
+//     // const localParse = JSON.parse(insertMessage);
+
+//     if (insertMessage) {
+
+
+//         console.log(insertMessage);
+//         refs.textarea.value = insertMessage;
+//     }
+
+// }
+
+const STORAGE_KEY = 'feedback-form-state';
+initForm();
+
+function onFormInput(event) {
+    let persistedFilter = localStorage.getItem(STORAGE_KEY);
+    persistedFilter = persistedFilter ? JSON.parse(persistedFilter) : {};
+    persistedFilter[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(persistedFilter));
+};
+
+function initForm() {
+    let persistedFilter = localStorage.getItem(STORAGE_KEY);
+    if (persistedFilter) {
+        persistedFilter = JSON.parse(persistedFilter);
+        Object.entries(persistedFilter).forEach(([name, value]) => {
+            refs.form.elements[nmae].value = value
+        });
     }
+};
 
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(refs.form);
+    const user = {};
+    formData.forEach((value, name) => {
+        user[name] = value;
+    });
+    console.log(user);
+    event.currentTarget.reset(); //очищяет форму
+    localStorage.removeItem(STORAGE_KEY); //очищяет localStorage
 }
